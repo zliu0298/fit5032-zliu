a356f881c70683d26d8f224afefdfc36052e675f
Zihan Liu <zliu0298@student.monash.edu>
2025-10-10 12:36:35 +1100

    Week10 eFolio: 10.2 Utilizing external API service

--- src/components/BHeader.vue
+++ src/components/BHeader.vue
@@ -54,6 +54,11 @@ const logout = async () => {
           </router-link>
         </li>
         <li class="nav-item">
+          <router-link to="/WeatherCheck" class="nav-link" active-class="active" aria-current="page">
+            Get Weather
+          </router-link>
+        </li>
+        <li class="nav-item">
           <button v-if="isAuthenticated" class="nav-link" @click="logout">Logout</button>
         </li>
       </ul>
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import LoginView from '@/views/LoginView.vue'
 import FirebaseSigninView from '../views/FirebaseSigninView.vue'
 import FirebaseRegisterView from '../views/FirebaseRegisterView.vue'
 import GetBookCountView from '../views/GetBookCountView.vue'
+import WeatherView from '../views/WeatherView.vue'
 import AddBookView from '../views/AddBookView.vue'
 import { isAuthenticated } from '@/authenticate'
 
@@ -44,6 +45,11 @@ const routes = [
     path: '/GetBookCount',
     name: 'GetBookCount',
     component: GetBookCountView
+  },
+  {
+    path: '/WeatherCheck',
+    name: 'WeatherCheck',
+    component: WeatherView
   },
   { 
     path: '/access-denied', 
